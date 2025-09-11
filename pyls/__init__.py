9637f1cc5c0fe6d19f50b31ccd50ebfdfd02f2b4
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2025-09-11 11:55:38 -0400

    [MNT] Modernize package structure

--- pyls/__init__.py
+++ pyls/__init__.py
@@ -8,9 +8,8 @@ __all__ = [
 
 ]
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from . import examples
 from .io import load_results, save_results
--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python
-import sys
-
 from setuptools import setup
 import versioneer
 
-SETUP_REQUIRES = ['setuptools >= 30.3.0']
-SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
-
-if __name__ == "__main__":
-    setup(name='pyls',
-          setup_requires=SETUP_REQUIRES,
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass())
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)
